eec228f6a70a39530a4ad37418a57748d5fafd43
Evgenii Babkin <evgeniy.babkin94@gmail.com>
2019-02-17 13:49:37 +0300

    layout

--- app/src/main/java/com/example/tweet/activity/SearchUserActivity.java
+++ app/src/main/java/com/example/tweet/activity/SearchUserActivity.java
@@ -46,7 +46,6 @@ public class SearchUserActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
         setContentView(R.layout.activity_search_user);
 
         initRecyclerView();
